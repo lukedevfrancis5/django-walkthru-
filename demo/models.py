95dec49dab7a7ec72908f3c379984a8222d584c7
luke francis <lukedevfrancis@gmail.com>
2023-07-08 18:29:02 -0400

    worked with choices field

--- demo/models.py
+++ demo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    STATUSES = {
+        (0, 'Unknown'),
+        (1, 'processed'),
+        (2, 'paid')
+
+
+    }
+    title = models.CharField(null=True, blank=False, choices=STATUSES) 
