934649d80e53714c374370fcab62a6ec898a8ece
luke francis <lukedevfrancis@gmail.com>
2023-07-11 22:21:45 -0400

    added views

--- demo/views.py
+++ demo/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.views import View 
+from .models import Book
 # Create your views here.
+
+class Another(View):
+    books = Book.objects.all()
+
+    book_num = f"There are {len(books)} books in the database"
+
+    def get(self, request):
+        return HttpResponse(self.book_num)
+
+def first(request):
+    return HttpResponse('First message from views')
