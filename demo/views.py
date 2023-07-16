9711ffc62d0d40a8ac14908603ad52ae8cdd0c6e
luke francis <lukedevfrancis@gmail.com>
2023-07-16 18:32:49 -0400

    removed a view

--- demo/views.py
+++ demo/views.py
@@ -2,19 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View 
 from .models import Book
+from django.shortcuts import render 
+
 # Create your views here.
 
-class Another(View):
-    book = Book.objects.get(id=1)
-
-    output = f"We have {book.title} book in the database with id {book.id}<br>"
-
-
-    #for book in books:
-    #    output += f"We have {book.title} book in the database with id {book.id}<br>"
-
-    def get(self, request):
-        return HttpResponse(self.output)
 
 def first(request):
-    return HttpResponse('First message from views')
+    books = Book.objects.all()
+
+    return render(request, 'first_temp.html', {'books': books})
